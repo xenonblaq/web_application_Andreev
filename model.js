81f372f96a5f2588fc9f13eb34c5b619daa2001a
Ivann Andreev <ivandreev@edu.hse.ru>
2024-05-19 01:24:35 +0300

    оптимизация и исправление ошибок модели

--- model.js
+++ model.js
@@ -8,7 +8,8 @@ class Company {
         this.liquidity = r;
         this.vol = vol;
         this.spread = buy - sell;
-        this.news_background = [news]
+        this.news_background = news
+        this.news = [news]
     }
 
 
@@ -22,29 +23,20 @@ class Company {
         this.vol = Math.sqrt(variations.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / 6);
     }
 
-    news_maker(avg) {
-        let P = Math.random();
-        if (avg > 0.6) {
-            if (P > 0.1) {
-                let news = Math.random() * (1 - 0.4) + 0.4;
-            } else {
-                let news = Math.random() * (0.6 - 0.1) + 0.1;
-            }
-        } else {
-            if (P > 0.1) {
-                let news = Math.random() * (0.6 - 0.1) + 0.1;
-            } else {
-                let news = Math.random() * (1 - 0.4) + 0.4;
-            }
+    news_maker() {
+        let news = Math.random();
+        this.news.push(news);
+        this.news_background = 0
+        for (let i = 0; i < this.news.length(); i++) {
+            this.news_background += slice[i] * Math.exp(0.9(i - this.news.length() + 1));
         }
-        return news
     }
 
     change_parametrs(buy_counter, sell_counter, deal_counter, step) {
         if (buy_counter > sell_counter) {
-            this.buy_price += Math.exp(buy_counter / 2 * sell_counter);
+            this.buy_price += Math.exp(buy_counter / (2 * sell_counter + 0.001));
         } else {
-            this.buy_price -= Math.exp(sell_counter / 2 * buy_counter);
+            this.buy_price -= Math.exp(sell_counter / (2 * buy_counter + 0.001));
         }
         this.graph_prices.push[this.buy_price];
         this.ret.push(Math.LN2(this.graph_prices[-1] / this.graph_prices[-2]));
@@ -59,33 +51,33 @@ class Company {
             this.liquidity = r;
             this.spread += Math.pow(0.5, 50 * r);
         }
-        if (this.news_background.length() < 5) {
-            const avg = this.news_background.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / this.news_background.length();
-        } else {
-            const slice = this.news_background.slice(-5);
-            const avg = slice.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / 5;
-        }
-        let news = this.news_maker(avg);
-        this.news_background.push(news);
+        this.sell_price = this.buy_price - this.spread;
+        this.news_maker();
     }
 }
 
 class Player {
     constructor(k, n, buy) {
-        this.parametrs = this.creating_parametrs();
+        this.parametrs = this.subjective_parametrs();
         this.profit = - k * buy / n;
         this.capital = k / n;
         this.graph_profits = [this.profit];
+        this.weights = []
     }
 
-    creating_parametrs() {
-       var parametrs = new Array(5);
+    subjective_parametrs() {
+       let weights;
+       let parametrs = new Array(5);
        parametrs[0] = Math.random(); // Риск
        parametrs[1] = this.profit; // Текущая прибыль
-       parametrs[2] = Math.random() * (10 - 0.2) + 0.2; // Опыт в годах
-       parametrs[3] = Math.random();
-       parametrs[4] = Math.random()
+       parametrs[2] = Math.random() * 9.8 + 0.2; // Опыт в годах
+       parametrs[3] = Math.random(); // отношение к сектору
+       parametrs[4] = Math.random(); // иное
        return parametrs;
+    }
+
+    changing_parametrs() {
+
     }
 
     scoring(factors, ret, vol, liq, news) {
@@ -93,26 +85,18 @@ class Player {
         for (let i = 0; i != 5; i++) {
             score += 0.5 * parseFloat(factors[i]);
         }
-        let recomend = "";
-        if (score >= 5.04) {
-            recomend = "buy";
-        } else if (score > 2.96 && score < 5.04) {
-            recomend = "hold";
-        } else {
-            recomend = "sell";
-        }
-        return recomend;
+        return score;
     }
 
 }
 
 export class Model {
-    constructor(days, n, k, buy, sell, r, news) {
+    constructor(days, n, k, buy, sell, r, news, ret, vol) {
         this.steps = days;
-        this.company = new Company(k, buy, sell, r, news);
+        this.company = new Company(k, buy, sell, r, news, ret, vol);
         this.players_number = n;
         this.players = this.create_players()
-        this.factors = [["up", 0.1, 0.9], ["up", -2 * k * buy / n, 2 * k * sell / n], ["up", 0.3, 9], ["down", 0.8, 0.3], ["up", 0.15, 0.97]]
+        this.factors = [[0.1, 0.9], [-2 * k * buy / n, 2 * k * sell / n], [0.3, 9], [0.8, 0.3], [0.15, 0.97]]
     }
 
     create_players() {
@@ -123,7 +107,7 @@ export class Model {
         return players;
     }
 
-    desirability_regres_func(factor, direction, terrible, ideal) {
+    desirability_regres_func(factor, terrible, ideal) {
         var desire = new Array(this.players_number);
         let minimum = Math.min(...factor);
         let maximum = Math.max(...factor);
@@ -131,32 +115,16 @@ export class Model {
         for (let i = 0; i != this.players_number; i++) {
             indexes[i] = factor[i];
         }
-        if (direction == "up") {
-            factor.sort((a, b) => a - b);
-            if (maximum - ideal >= 0) {
-                maximum = 1;
-            } else {
-                maximum = maximum / ideal;
-            }
-            if (minimum - terrible < 0) {
-                minimum = 0;
-            } else {
-                minimum = minimum / ideal;
-            }
+        factor.sort((a, b) => a - b);
+        if (maximum - ideal >= 0) {
+            maximum = 1;
         } else {
-            factor.sort((a, b) => b - a);
-            minimum = Math.max(...factor);
-            if (terrible - minimum < 0) {
-                minimum = 0;
-            } else {
-                minimum = ideal / minimum;
-            }
-            maximum = Math.min(...factor);
-            if (maximum - ideal <= 0) {
-                maximum = 1;
-            } else {
-                maximum = ideal / maximum;
-            }
+            maximum = maximum / ideal;
+        }
+        if (minimum - terrible < 0) {
+            minimum = 0;
+        } else {
+            minimum = minimum / ideal;
         }
         var normalized = factor.map((_, index) => (minimum + index * (maximum - minimum) / (this.players_number - 1)));
         const sumX = factor.reduce((acc, val) => acc + val, 0);
@@ -185,56 +153,42 @@ export class Model {
             scoring_matrix[i] = this.desirability_regres_func(scoring_matrix[i], this.factors[i][0], this.factors[i][1], this.factors[i][2]);
         }
         for (let i = 0; i != this.players_number; i++) {
-            final_recomend[i] = this.players[i].scoring(scoring_matrix.map(row => row[i]));
+            final_recomend[i] = [this.players[i].scoring(scoring_matrix.map(row => row[i]), this.company.ret[-1],
+            this.company.liquidity, this.company.vol, this.company.news_background), i];
         }
         return final_recomend;
     }
 
     deal(final_recomend) {
-        var deque = new Array(this.players_number).fill(0);
-        let deal_counter = 0
+        var deque_sell = [];
+        var deque_buy = [];
+        final_recomend.sort();
         for (let i = 0; i != this.players_number; i++) {
-            if (final_recomend[i] == "sell") {
-                let b_idx = deque.indexOf("buy");
-                if (b_idx != 1) {
-                    this.players[b_idx].capital += this.company.stock_number / this.players_number;
-                    this.players[b_idx].profit -= this.company.stock_number * this.company.buy_price / this.players_number;
-                    this.players[i].capital -= this.company.stock_number / this.players_number;
-                    this.players[i].profit += this.company.stock_number * this.company.sell_price / this.players_number;
-                    deal_counter += this.company.stock_number;
-                    deque[b_idx] = 0;
-                } else {
-                    deque[i] = "sell";
-                }
-            } else {
-                let s_idx = deque.indexOf("sell");
-                if (s_idx != -1) {
-                    this.players[s_idx].capital -= this.company.stock_number / this.players_number;
-                    this.players[s_idx].profit += this.company.stock_number * this.company.sell_price / this.players_number;
-                    this.players[i].capital += this.company.stock_number / this.players_number;
-                    this.players[i].profit -= this.company.stock_number * this.company.buy_price / this.players_number;
-                    deal_counter += this.company.stock_number;
-                    deque[s_idx] = 0;
-                } else {
-                    deque[i] = "buy";
-                }
+            if (final_recomend[i][0] >= 5.04) {
+                deque_buy.push(final_recomend[i][1]);
+            } else if (final_recomend[i][0] <= 2.96 && this.players[i].capital > 0) {
+                deque_sell.push(final_recomend[i][1]);
             }
-            this.players[i].graph_profits.push(this.players[i].profit);
         }
-        return deal_counter;
+        let idx_sell = 0;
+        let idx_buy = deque_buy.length();
+        while (idx_buy != 0 && idx_sell != deque_sell.length()) {
+            this.players[deque_buy(idx_buy)].capital += this.players[deque_sell(idx_sell)].capital;
+            this.players[deque_buy(idx_buy)].profit -= this.company.buy_price * this.players[deque_sell(idx_sell)].capital;
+            this.players[deque_sell(idx_sell)].profit += this.company.sell_price * this.players[deque_sell(idx_sell)].capital;
+            this.players[deque_buy(idx_buy)].graph_profits.push(this.players[deque_buy(idx_buy)].profit);
+            this.players[deque_sell(idx_sell)].graph_profits.push(this.players[deque_sell(idx_sell)].profit);
+            deal_counter += this.players[deque_sell(idx_sell)].capital;
+            this.players[deque_sell(idx_sell)].capital = 0;
+            idx_buy -= 1;
+            idx_sell = 1;
+        }
+        return [deal_counter, deque_buy.length(), deque_sell.length()];
     }
 
     change_price(final_recomend, step) {
-        let sell_counter = 0;
-        let buy_counter = 0;
-        for (let i = 0; i != this.players_number; i++) {
-            if (final_recomend[i] == "buy") {
-                buy_counter += 1;
-            } else if (final_recomend[i] == "sell") {
-                sell_counter += 1;
-            }
-        }
-        let news = this.company.change_parametrs(buy_counter, sell_counter, this.deal(final_recomend), step);
+        var deal = this.deal(final_recomend);
+        let news = this.company.change_parametrs(deal[1], deal[2], deal[0], step);
     }
 
     run() {
